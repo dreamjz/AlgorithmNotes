d485e906b44b22b1afd3bdd7480ae70b0ffbc324
kesa <2356216441@qq.com>
2022-03-11 14:32:16 +0800

    LeetCode: Array - 1. Two-Sum

---
+++ LeetCode/Array/1.Two-Sum/Two-Sum.go
@@ -0,0 +1,24 @@
+package array
+
+// bruteForce 暴力解法
+func bruteForce(nums []int, target int) []int {
+	for i, x := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if x+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+func hashTable(nums []int, target int) []int {
+	hashTable := map[int]int{}
+	for i, x := range nums {
+		if j, ok := hashTable[target-x]; ok {
+			return []int{i, j}
+		}
+		hashTable[x] = i
+	}
+	return nil
+}
---
+++ LeetCode/Array/1.Two-Sum/Two-Sum_test.go
@@ -0,0 +1,81 @@
+package array
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Question1 struct {
+	params Param
+	ans    []int
+}
+
+type Param struct {
+	nums   []int
+	target int
+}
+
+var (
+	qs1 []Question1
+)
+
+const (
+	errFormat  = "Wrong Answer: [input]: %v, [output]: %v, [answer]: %v\n"
+	passFormat = "Pass: [input]: %v, [output]: %v, [answer]: %v\n"
+)
+
+func init() {
+	qs1 = []Question1{
+		{
+			params: Param{[]int{2, 7, 11, 15}, 9},
+			ans:    []int{0, 1},
+		},
+		{
+			params: Param{[]int{3, 2, 4}, 6},
+			ans:    []int{1, 2},
+		},
+		{
+			params: Param{[]int{3, 3}, 6},
+			ans:    []int{0, 1},
+		},
+	}
+}
+
+func TestBruteForce(t *testing.T) {
+	for _, q := range qs1 {
+		res := bruteForce(q.params.nums, q.params.target)
+		if !intsEqWithoutOrder(res, q.ans) {
+			t.Errorf(errFormat, q.params, res, q.ans)
+		}
+		fmt.Printf(passFormat, q.params, res, q.ans)
+	}
+}
+
+func TestHashTable(t *testing.T) {
+	for _, q := range qs1 {
+		res := hashTable(q.params.nums, q.params.target)
+		if !intsEqWithoutOrder(res, q.ans) {
+			t.Errorf(errFormat, q.params, res, q.ans)
+		}
+		fmt.Printf(passFormat, q.params, res, q.ans)
+	}
+}
+
+func intsEqWithoutOrder(a, b []int) bool {
+	if (a == nil) != (b == nil) {
+		return false
+	}
+	if len(a) != len(b) {
+		return false
+	}
+	hashMap := map[int]int{}
+	for i, v := range a {
+		hashMap[v] = i
+	}
+	for _, v := range b {
+		if _, ok := hashMap[v]; !ok {
+			return false
+		}
+	}
+	return true
+}
